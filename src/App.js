69faa849e8351e8c3136ab9f875dbfa2644c95b4
Sarrthi Jasrotia <sarrthijasrotia05@gmail.com>
2022-09-20 22:09:30 -0500

    trafered the code from codeSandBox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
+import weatherData from "./weatherData";
+import WeatherForecast from "./components/WeatherForecast";
 
-function App() {
+const WeatherForecastCards = weatherData.map((ele, index) => {
+  return <WeatherForecast {...ele} />;
+});
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My WeatherIcons App</h1>
+      <section>{WeatherForecastCards}</section>
     </div>
   );
 }
-
-export default App;
